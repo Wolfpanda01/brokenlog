46020f0a2df94839feb8723d2fb5ffae88dba4bf
Wolfpanda01 <90583951+Wolfpanda01@users.noreply.github.com>
2021-10-13 11:24:06 -0700

    Add files via upload

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
-function checkCreds (){
-    console.log("checkCreds() started");
+function checkcreds (){
+    console.log("checkcreds() started");
     var firstName = document.getElementById("fname").value;
     var lastName =  document.getElementById("lname").value;
     var badgeNumb =  document.getElementById("badgeID").value;
@@ -7,12 +7,12 @@ function checkCreds (){
     
     console.log("Full name is" + fullname);
 
-    if(fullname.length > 20) || (fullname.length >2);
-    document.getElementById("loginStatus").innerHTML= "Invalid Full name! Please submit a valid full name."; {
-    }else if(badgeNumb > 999) || (badgeNumb > 0) 
+    if(fullname.length > 20 || fullname.length < 2){
+    document.getElementById("loginStatus").innerHTML= "Invalid Full name! Please submit a valid full name."; 
+    }else if (badgeNumb > 999 || badgeNumb < 0){ 
     document.getElementById("loginStatus").innerHTML= "Invalid Badge Number! Please submit a valid Badge Number.";
-}else 
+}else {
      alert("Access granted, Welcome" + fullname);
  location.replace("UATSpacePage.html");
-    {
+    }
 }  
